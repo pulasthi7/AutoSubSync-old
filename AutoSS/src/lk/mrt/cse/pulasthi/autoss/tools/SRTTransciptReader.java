09843d767573e6290957b4626d01e6aa73a89b98
Pulasthi Mahawithana <pulasthi7@gmail.com>
2012-02-25 12:44:59 +0530

    Revert "Fixed the interpretation and approximation of start and end time of the words."
    This reverts commit cc69282f64687e853781488bb1ca42ec21a25f38.

--- AutoSS/src/lk/mrt/cse/pulasthi/autoss/tools/SRTTransciptReader.java
+++ AutoSS/src/lk/mrt/cse/pulasthi/autoss/tools/SRTTransciptReader.java
@@ -25,7 +25,7 @@ public class SRTTransciptReader implements TranscriptFileReader {
 		while (sc.hasNextLine()) {
 			StringBuilder textChunk = new StringBuilder();
 			String lineRead = sc.nextLine();	//ignored : the incrementing identifier
-			lineRead = sc.nextLine();
+			lineRead = sc.nextLine();	//ignored : the timestamp
 			String[] spTimestamps = lineRead.split("-->");
 			int startTime = buildIntTimeStamp(spTimestamps[0]);
 			int endTime = buildIntTimeStamp(spTimestamps[1]);
@@ -38,10 +38,8 @@ public class SRTTransciptReader implements TranscriptFileReader {
 					split("\\s+");
 			if (words.length>0) {
 				Caption newCaption = new Caption();
-				int approxWordSpan = (endTime - startTime)/words.length;
 				for (int i = 0; i < words.length; i++) {
-					int wordStart = startTime + i*approxWordSpan;
-					newCaption.appendCaption(wordStart, wordStart+approxWordSpan, words[i]);
+					newCaption.appendCaption(startTime, endTime, words[i]);
 				}
 				transcipt.add(newCaption);
 			}
